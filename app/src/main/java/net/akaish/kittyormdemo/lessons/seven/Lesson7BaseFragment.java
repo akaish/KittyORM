8a1cdc9fada1295617829e3388825f78607cdb22
akaish (Denis Bogomolov) <akaish@mail.ru>
2019-01-13 06:13:28 +0300

    Swithing to APACHE v2 license: adding to sources license text

--- app/src/main/java/net/akaish/kittyormdemo/lessons/seven/Lesson7BaseFragment.java
+++ app/src/main/java/net/akaish/kittyormdemo/lessons/seven/Lesson7BaseFragment.java
@@ -4,12 +4,11 @@
  *
  *  Copyright (c) 2018 Denis Bogomolov (akaish)
  *
- * This work is licensed under a
- * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://creativecommons.org/licenses/by-nc-nd/4.0/
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
